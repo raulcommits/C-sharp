53a2cf19bdafbe6ffd7cd045fa671a825cfd4651
raulcommits <raultomaz08@gmail.com>
2024-10-21 20:37:01 -0300

    Projeto 21-10

---
+++ Atividade 20 (Vetores - Nomes)/Atividade 20 (Vetores - Nomes)/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno
+ * Date: 21/10/2024
+ * Time: 19:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Atividade_20__Vetores___Nomes_
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(60, 26);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(291, 444);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(402, 213);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(110, 63);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Nomys e\r\nSahlarioz";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SeaGreen;
+			this.ClientSize = new System.Drawing.Size(569, 482);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "MainForm";
+			this.Text = "Atividade 20 (Vetores - Nomes)";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Atividade 20 (Vetores - Nomes)/Atividade 20 (Vetores - Nomes)/MainForm.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: aluno
+ * Date: 21/10/2024
+ * Time: 19:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Atividade_20__Vetores___Nomes_
+{
+
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+
+			InitializeComponent();
+			
+
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+			string [] nomes = {"Ãnah", "Brouno", "Kharlos", "Dyiogo", "Luiz", "Marcos", "Rauhl", "Uesllei", "Djhenyffer", "Hesmeralda", "Firmino", "Hector", "Gustavo", "Ysadora", "Kauã", "Lorehnna", "Matilda", "Nycolaz", "Otávyo", "Pedro"};
+			float [] salarios = {2000.00f, 2115.00f, 3000.00f, 1450.00f, 9000.00f, 4000.00f, 7600.00f, 8000.00f, 3500.00f, 4030.00f, 5000.21f, 6000.10f, 6400.00f, 2800.00f, 7100.70f, 5040.00f, 8000.00f, 1100.00f, 2900.08f, 3300.40f};
+			for (int i=0; i<20; i++)
+			{
+				listBox1.Items.Add((i+1) + " - " + nomes[i] + ": " + "Salário " + " - " + salarios[i].ToString("C"));
+			}
+		}
+	}
+}
